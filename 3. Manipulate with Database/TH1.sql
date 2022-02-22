3666d1961fdfe2cf41f9fa0edde8106a6065b2b5
cuongng22 <theendvg2003@gmail.com>
2022-02-22 16:44:46 +0700

    add

---
+++ 3. Manipulate with Database/TH1.sql
@@ -0,0 +1,64 @@
+create database quanlysinhvien;
+
+use quanlysinhvien;
+
+
+create table class(
+classid int not null primary key auto_increment,
+classname varchar(60) not null,
+startdate datetime not null,
+status bit
+);
+
+create table student(
+studentid int not null auto_increment primary key,
+studentname varchar(30) not null,
+address varchar(20),
+phone varchar(20),
+status bit not null,
+classid int not null
+);
+
+create table subject(
+subid int not null primary key auto_increment,
+subname varchar(30) not null,
+Credit tinyint not null default 1 check(credit>=1),
+Status  BIT  DEFAULT 1
+);
+
+CREATE TABLE Mark
+(
+    MarkId    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    SubId     INT NOT NULL,
+    StudentId INT NOT NULL,
+    Mark      FLOAT   DEFAULT 0 CHECK ( Mark BETWEEN 0 AND 100),
+    ExamTimes TINYINT DEFAULT 1,
+    UNIQUE (SubId, StudentId),
+    FOREIGN KEY (SubId) REFERENCES Subject (SubId),
+    FOREIGN KEY (StudentId) REFERENCES Student (StudentId)
+);
+
+INSERT INTO Class
+VALUES (1, 'A1', '2008-12-20', 1);
+INSERT INTO Class
+VALUES (2, 'A2', '2008-12-22', 1);
+INSERT INTO Class
+VALUES (3, 'B3', current_date, 0);
+
+INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
+VALUES ('Hung', 'Ha Noi', '0912113113', 1, 1);
+INSERT INTO Student (StudentName, Address, Status, ClassId)
+VALUES ('Hoa', 'Hai phong', 1, 1);
+INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
+VALUES ('Manh', 'HCM', '0123123123', 0, 2);
+
+INSERT INTO Subject
+VALUES (1, 'CF', 5, 1),
+       (2, 'C', 6, 1),
+       (3, 'HDJ', 5, 1),
+       (4, 'RDBMS', 10, 1);
+       
+INSERT INTO Mark (SubId, StudentId, Mark, ExamTimes)
+VALUES (1, 1, 8, 1),
+       (1, 2, 10, 2),
+       (2, 1, 12, 1);
--- Exercise/Bai2.sql
+++ Exercise/Bai2.sql
@@ -12,7 +12,7 @@ phone varchar(10) not null
 );
 
 create table classes(
-id int auto_increment primary key,
+id int auto_increment not null primary key,
 name varchar(50) not null unique
 );
 
@@ -20,7 +20,7 @@ alter table  students
 add column classid int;
 
 alter table students
-add foreign key(classid) references classes(id);
+add constraint foreign key(classid) references classes(id);
 insert into classes(name) value
 ('c10'),
 ('c11'),
@@ -30,17 +30,12 @@ insert into classes(name) value
 
 select * from classes;
 
-insert into students(name, address, sex, age, email, phone ) value
-("cuong","HN","1","19","cuong@gmail.com","0977183350"),
-("cuong1","HN","1","20","cuong1@gmail.com","0977183351"),
-("cuong2","HN","0","21","cuong2@gmail.com","0977183352"),
-("cuong3","HN","1","19","cuong3@gmail.com","0977183353"),
-("cuong4","HN","1","19","cuong4@gmail.com","0977183354"),
-("cuong5","HN","0","23","cuong5@gmail.com","0977183355"),
-("cuong6","HN","1","19","cuong6@gmail.com","0977183356"),
-("cuong7","HN","1","19","cuong7@gmail.com","0977183357"),
-("cuong8","HN","1","25","cuong8@gmail.com","0977183358"),
-("cuong9","HN","1","19","cuong9@gmail.com","0977183359");
+insert into bai2.students(name, address, sex, age, email, phone, classid ) value
+("cuong","HN","1","19","cuong@gmail.com","0977183350", "1"),
+("cuong2","HN","0","21","cuong2@gmail.com","0977183352", "3"),
+("cuong4","HN","1","19","cuong4@gmail.com","0977183354", "1"),
+("cuong6","HN","1","19","cuong6@gmail.com","0977183356", "2"),
+("cuong7","HN","1","19","cuong7@gmail.com","0977183357", "1");
 
 select * from students;
 
@@ -48,7 +43,90 @@ select * from students order by name desc;
 
 select * from students order by age;
 
-update students set classid = '1' where (id = '1');
+drop table classes;
+drop table students;
+
+select students.classid, classes.name, count(*) as "so luong hoc sinh"
+from studens, classes
+where students.classid = classes.id
+group by studens.classid, classes.name;
+
+SELECT students.classId, classes.name, COUNT(*) AS 'Số lượng học sinh: '
+FROM bai2.students, bai2.classes
+where students.classId = classes.id
+group by students.classId, classes.name;
+
+select address, count(address) as "slhs"
+from students 
+group by students.address;
+use bai2;
+
+drop table students;
+
+
+SELECT students.ClassId , classes.Name , COUNT(*) AS totalStudent
+FROM bai2.students, bai2.classes
+where students.ClassId = classes.Id 
+group by students.ClassId, classes.name
+having totalStudent = 
+(select max(a.totalStudent) from 
+(select students.ClassId , classes.Name , COUNT(*) AS totalStudent
+from bai2.students, bai2.classes
+where students.ClassId = classes.Id
+group by students.ClassId, classes.name)a);
+
+select students.classid, classes.name, count(*) AS totalStudent
+from bai2.students 
+join classes on students.classid = classes.id
+group by students.classid, classes.name;
+
+SELECT students.ClassId , classes.Name , COUNT(*) AS totalStudent
+FROM bai2.students, bai2.classes
+where students.ClassId = classes.Id 
+group by students.ClassId, classes.name
+having totalStudent = 
+(select min(a.totalStudent) from 
+(select students.ClassId , classes.Name , COUNT(*) AS totalStudent
+from bai2.students, bai2.classes
+where students.ClassId = classes.Id
+group by students.ClassId, classes.name)a);
+
+select students.classid, classes.name, count(*) as total
+from bai2.students, bai2.classes
+where students.classid = classes.id
+group by students.classid, classes.id
+having total >= 5;
+
+select students.address, count(students.address) as total
+from bai2.students
+group by students.address
+having total >=5;
+
+select students.classid, students.name, classes.name, students.age
+from bai2.students, bai2.classes
+where age = (select min(age) from bai2.students);
+
+select students.classid, students.name, classes.name, students.age
+from bai2.students, bai2.classes
+where age = (select max(age) from bai2.students);
+
+delete from bai2.students
+where students.id in(
+select * from (
+select students.id from bai2.students where (address like "HN")
+) a
+);
+
+update bai2.students set sex = "0" where students.id in (
+select students.id 
+from bai2.students 
+where( students.address like "HN")
+);
+
+select * from bai2.students;
+
+
+
 
 
 
